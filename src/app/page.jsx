68dddbaad0dd345776d209bac7eeed30e1ddcce9
Estevão Amaro <141499542+Steve09012004@users.noreply.github.com>
2025-06-27 14:30:58 -0300

    adicionando perguntas ao json e melhorando design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,29 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.css";
 import TopQuestion from "./components/topQuestion";
+import Button from "./components/button";
+import React from "react";
+import { useState } from "react";
+
+
 
 export default function Home() {
+
+  function verificarResposta() {
+      setEnunciado(enunciado + 1)
+  }
+
+  const questions = require('./questions.json')
+
+  const [enunciado, setEnunciado] = useState(0)
+
   return (
     <main>
       <div className="boxLeft">
-          <TopQuestion />
+          <TopQuestion  question={questions[enunciado]}/>
+          
+          <Button onClick={verificarResposta}/>
       </div>
       <div className="boxRight">
 
