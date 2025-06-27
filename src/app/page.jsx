1b1b5d33a926b404c64a6376ea65d41412b94743
Estevão Amaro <141499542+Steve09012004@users.noreply.github.com>
2025-06-27 16:40:11 -0300

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "./page.css";
 import TopQuestion from "./components/topQuestion";
 import Button from "./components/button";
+import Respostas from "./components/respostas";
 import React from "react";
 import { useState } from "react";
 
@@ -10,18 +11,34 @@ import { useState } from "react";
 
 export default function Home() {
 
-  function verificarResposta() {
+  function verificarResposta(indice) {
+    if (perguntaAtual.resposta == perguntaAtual.alternativas[indice]){
       setEnunciado(enunciado + 1)
+    }else {
+      alert("Errou")
+    }
   }
 
   const questions = require('./questions.json')
 
   const [enunciado, setEnunciado] = useState(0)
+  const perguntaAtual = questions[enunciado]
 
   return (
     <main>
       <div className="boxLeft">
-          <TopQuestion  question={questions[enunciado]}/>
+          <TopQuestion  question={perguntaAtual}/>
+
+          <div className="medBox">
+            {perguntaAtual.alternativas.map((alternativa, index) => (
+              <Respostas 
+                key={index}
+                alternativa={alternativa}
+                indice = {index}
+                onClick={verificarResposta}
+              />
+            ))}
+          </div>
           
           <Button onClick={verificarResposta}/>
       </div>
