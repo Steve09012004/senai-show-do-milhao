e8fba80e67a72c8fa1cc916367c63239b0a2cbb9
Estevão Amaro <141499542+Steve09012004@users.noreply.github.com>
2025-06-27 14:29:17 -0300

    Add files via upload

---
+++ src/app/components/button.jsx
@@ -0,0 +1,11 @@
+
+export default function Button({onClick}) {
+
+
+
+    return (
+        <div id="button">
+            <button onClick={onClick}>CONFIRMAR</button>
+        </div>
+    )
+}
--- src/app/components/topQuestion.jsx
+++ src/app/components/topQuestion.jsx
@@ -1,10 +1,15 @@
 
-export default function TopQuestion() {
+export default function TopQuestion({question}) {
+
+
+
     return (
         <div className="topBox">
             <img className="logo" src="/images/logo.png" alt="logo" />
-            <div className="pergunta">
-                <p>Quem descobriu o Brasil</p>
+            <div className="perguntas">
+                <div className="pergunta">
+                    <p>{question.enunciado}</p>
+                </div>
             </div>
         </div>
     )
